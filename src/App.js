3bb171cfbd0e08a1e34bec85d2ad98eb1dbbaae6
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-05-30 09:33:40 -0400

    added modal via react portal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const showModalHandler = () => {
+    setShowModal(true);
+  };
+
+  const hideModalHandler = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
-      <Header />
+      {showModal && <Cart onClose={hideModalHandler} />}
+      <Header onShow={showModalHandler} />
       <main>
         <Meals />
       </main>
